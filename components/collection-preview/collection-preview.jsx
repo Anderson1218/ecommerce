7723a34ecb01b3cbbe61056ac23e6b5560ec8632
Anderson <anderson771218@gmail.com>
2020-05-31 23:38:02 +0800

    abcd

--- components/collection-preview/collection-preview.jsx
+++ components/collection-preview/collection-preview.jsx
@@ -3,7 +3,7 @@ import {
   PreviewContainer,
   Preview,
   Item,
-  Title
+  Title,
 } from "./collection-preview.styles";
 import Card from "../card/card";
 import Link from "next/link";
@@ -11,12 +11,12 @@ import Link from "next/link";
 const CollectionPreview = ({ title, items, routeName }) => (
   <PreviewContainer>
     <Link href="/collections/[collection]" as={`/collections/${routeName}`}>
-      <Title>{`More ${title.toUpperCase()}`}&#10132;</Title>
+      <Title>{`MoreMore ${title.toUpperCase()}`}&#10132;</Title>
     </Link>
     <Preview>
       {items
         .filter((item, idx) => idx < 4)
-        .map(item => (
+        .map((item) => (
           <Item key={item.id}>
             <Card item={item} />
           </Item>
